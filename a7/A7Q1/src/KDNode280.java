bb439cc09d1fcc73bce02ac688b96efe1e7a1f0a
tyrelkostyk <tck290@mail.usask.ca>
2021-03-30 13:28:37 -0600

    first step of A7Q1

--- a7/A7Q1/src/KDNode280.java
+++ a7/A7Q1/src/KDNode280.java
@@ -1,2 +1,34 @@
-public class KDNode280 {
+import lib280.base.NDPoint280;
+
+public class KDNode280 extends NDPoint280 {
+
+
+    /**
+     * Create a new N-dimensional Node at the origin (0,0,..., 0).
+     * @param dim Dimensionality of the point.
+     */
+    public KDNode280(int dim) {
+        super(dim);
+    }
+
+    /**
+     * Create a point from a coordinate in an array of Double.  The dimensionality of
+     * the point is equal to the length of pt.
+     *
+     * @param pt Coordinates of the new point.
+     */
+    public KDNode280(Double pt[]) {
+        super(pt);
+    }
+
+    /**
+     * Create a point from a coordinate in array of double.  The dimensionality of
+     * the point is equal to the length of pt.
+     *
+     * @param pt Coordinates of the new point.
+     */
+    public KDNode280(double pt[]) {
+        super(pt);
+    }
+
 }
--- a7/A7Q1/src/KDTree.java
+++ a7/A7Q1/src/KDTree.java
@@ -1,2 +1,21 @@
+
+
 public class KDTree {
+    KDNode280 rootNode;
+
+    public void KDTree(int dim) {
+        rootNode = new KDNode280(dim);
+    }
+
+    public void KDTree(Double pts[]) {
+        rootNode = new KDNode280(pts);
+    }
+
+    public void KDTree(double pts[]) {
+        rootNode = new KDNode280(pts);
+    }
+
+    public static void main(String args[]) {
+
+    }
 }
