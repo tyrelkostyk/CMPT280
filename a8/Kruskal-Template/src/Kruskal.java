c62ab49f7fe73e9873be6437d6093c50bc8df926
tyrelkostyk <tck290@mail.usask.ca>
2021-04-11 20:12:16 -0600

    added starter code for a8

---
+++ a8/Kruskal-Template/src/Kruskal.java
@@ -0,0 +1,27 @@
+import lib280.graph.Vertex280;
+import lib280.graph.WeightedEdge280;
+import lib280.graph.WeightedGraphAdjListRep280;
+import lib280.tree.ArrayedMinHeap280;
+
+public class Kruskal {
+	
+	public static WeightedGraphAdjListRep280<Vertex280> minSpanningTree(WeightedGraphAdjListRep280<Vertex280> G) {
+
+		// TODO -- Complete this method.
+		
+		return null;  // Remove this when you're ready -- it is just a placeholder to prevent a compiler error.
+	}
+	
+	
+	public static void main(String args[]) {
+		WeightedGraphAdjListRep280<Vertex280> G = new WeightedGraphAdjListRep280<Vertex280>(1, false);
+		G.initGraphFromFile("Kruskal-template/mst.graph");
+		System.out.println(G);
+		
+		WeightedGraphAdjListRep280<Vertex280> minST = minSpanningTree(G);
+		
+		System.out.println(minST);
+	}
+}
+
+
